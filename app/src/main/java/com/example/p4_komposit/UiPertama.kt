c01803b405389610e3397cce5869532154211094
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-10-24 15:20:12 +0700

    menambahkan Card dulu karena kode di depan ruangan belum dikebawahin. dan menambahkan logo umy untuk nanti

--- app/src/main/java/com/example/p4_komposit/UiPertama.kt
+++ app/src/main/java/com/example/p4_komposit/UiPertama.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,5 +32,9 @@ fun ActivitasPertama(modifier: Modifier){
         )
 
         Spacer(modifier = Modifier.height(height = 25.dp))
+
+        Card(
+            modifier = Modifier
+        ) {  }
     }
 }
