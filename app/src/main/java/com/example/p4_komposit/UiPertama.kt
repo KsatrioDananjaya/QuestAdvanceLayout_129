137671ea6390e488819c97aee680949cf013cf4b
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-10-24 15:39:14 +0700

    menambahkan text alamat dibawah nama

--- app/src/main/java/com/example/p4_komposit/UiPertama.kt
+++ app/src/main/java/com/example/p4_komposit/UiPertama.kt
@@ -74,7 +74,12 @@ fun ActivitasPertama(modifier: Modifier){
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
-
+                    Text(
+                        stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(10.dp)
+                    )
                 }
             }
         }
