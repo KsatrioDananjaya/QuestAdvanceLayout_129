92434524ce80425e16751f54164cec9ad604f7df
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-10-25 11:10:27 +0700

    Menambahkan variable icons dan gambar profil

--- app/src/main/java/com/example/p4_komposit/UiPertama.kt
+++ app/src/main/java/com/example/p4_komposit/UiPertama.kt
@@ -1,6 +1,7 @@
 package com.example.p4_komposit
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,13 +12,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -28,9 +32,23 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun ActivitasPertama(modifier: Modifier){
     Column(modifier = Modifier
-        .padding(top=100.dp)
+        .padding(start = 24.dp, end = 24.dp, top = 60.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally) {
+        val profil = painterResource(id = R.drawable.profil)
+        val steam = painterResource(id = R.drawable.steam)
+        val disc = painterResource(id = R.drawable.discord)
+        val git = painterResource(id = R.drawable.github)
+        Image(
+            painter = profil,
+            null,
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.CenterEnd,
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.White, CircleShape)
+        )
         Text(
             text = stringResource(id = R.string.prodi),
             fontSize = 30.sp,
